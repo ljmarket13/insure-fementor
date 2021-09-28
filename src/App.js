37d16a25ea54fdaa7c2a39d2183e38fa27fb102c
ljmarket13 <ljmarket13@gmail.com>
2021-09-28 16:46:49 -0600

    added bottom banner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Card from './components/Card.jsx';
 import Header from './components/Header.jsx';
 import Navbar from './components/Navbar.jsx';
+import BannerBottom from './components/BannerBottom.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 			<Navbar />
 			<Header />
 			<Card/>
+			<BannerBottom />
     </div>
   );
 }
--- src/components/BannerBottom.jsx
+++ src/components/BannerBottom.jsx
@@ -1,9 +1,15 @@
+import bottomBanner from '../images/desktop/bg-pattern-how-we-work-desktop.svg';
 
 
 const BannerBottom = () => {
 	return (
-		<div>
-			
+		<div className="bottomContainer">
+			<div className="bottomBanner">
+				<h2>Find out more 
+				<br/>
+				about how we work</h2>
+			</div>
+			<button className="bottomBtn">how we work</button>	
 		</div>
 	)
 }
