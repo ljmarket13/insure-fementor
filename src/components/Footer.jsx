0b58490e8cb09a0eb3eea8670cc4793bc76666e0
ljmarket13 <ljmarket13@gmail.com>
2021-09-29 17:05:07 -0600

    completed footer component. refactor code next

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,36 @@ import { useState } from 'react';
  
 const Footer = () => {
 
-	const [icon, setIcon] = useState(icons)
+	const [icon, setIcon] = useState(icons);
+	const [footText, setFootText] = useState(foot);
 
 	return (
 		<div>
 			<footer>
-				<div className="footerIcon">
-				<img src={logo} alt="logo" className="footerLogo" />
-					{icons.map(({id, icon}) => (				
-						<div key={id} className="icon">
-							<img src={icon} alt="icon" />
-						</div>
-					))}
+				<div className="footerLogo">
+					<img src={logo} alt="logo"  />	
+					<div className="footerIcon">	
+						{icon.map(({id, icon}) => (				
+							<div key={id} className="icon">
+								<img src={icon} alt="icon" />
+							</div>
+						))}
+					</div>
 				</div>
+				<div className="footerText">
+					{footText.map(({id, title, list1, list2, list3, list4}) => (
+						<div key={id}>
+							<ul className="footerUl">
+      		  		<li>{title}</li>
+      		  		<li><a href="#">{list1}</a></li>
+      		  		<li><a href="#">{list2}</a></li>
+      		  		<li><a href="#">{list3}</a></li>
+      		  		<li><a href="#">{list4}</a></li>
+      				</ul>
+						</div>
+
+					))}
+				</div>	
 			</footer>
 		</div>
 	)
